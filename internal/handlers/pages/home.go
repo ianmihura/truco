527470976943373f686e1d3f69be669e1cfd1c0f
Ian Mihura <mihura.ian@gmail.com>
2026-02-02 16:50:46 +0100

    send cards to frontend

--- internal/handlers/pages/home.go
+++ internal/handlers/pages/home.go
@@ -31,6 +31,7 @@ func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// First default action
 	match := fsm.NewMatch()
 	trackerData := partials.TrackerData{
 		ActionTitle: "Jugador 1",
@@ -38,14 +39,17 @@ func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		State:       string(match.Encode()),
 	}
 
+	// Initial load of all cards
+	cards := partials.GetAvailableCards(truco.FilterHands{})
+
 	data := struct {
 		Stats   template.JS
 		Tracker partials.TrackerData
-		// Cards   []partials.CardUI
+		Cards   []partials.CardUI
 	}{
 		Stats:   template.JS(statsJSON),
 		Tracker: trackerData,
-		// Cards:   partials.GetAvailableCards(match.GetStatsFilter()),
+		Cards:   cards,
 	}
 
 	if err := h.Tmpl.ExecuteTemplate(w, "index.html", data); err != nil {
